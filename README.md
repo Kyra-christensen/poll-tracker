20240fca618d9037251e3a343d1532dc1c74fcea
Kyra Christensen <christensen.kyral@gmail.com>
2021-12-08 10:32:23 -0800

    completed poll tracker

--- README.md
+++ README.md
@@ -23,3 +23,31 @@ Additional considerations:
   - What needs to live in a persistence layer?
 - Is there some state we need to initialize?
 - Ask: should any of this work be abstracted into functions? (i.e., is the work complicated? can it be resused?)
+
+6 buttons
+
+option A add button
+Why? 💥💥💥 Add event listener to increment the vote count for option A in the current poll
+option B add button
+Why? 💥💥💥 Add event listener to increment the vote count for option B in the current poll
+option A subtract/undo button
+Why? 💥💥💥 Add event listener to decrement the vote count for option A in the current poll
+option B subtract/undo buttons
+Why? 💥💥💥 Add event listener to decrement the vote count for option B in the current poll
+create poll form
+Why? Add 'submit' listener to (not a click listener) to the form itself!
+three inputs (question, option 1, option 2)
+Why? Figure out the user's question and options
+Why? 💥💥💥 On submit, update the current poll question and options, and reflect that in the DOM
+1 'close poll' button
+Why?
+💥💥💥
+Append the current poll to the past polls state []
+"Update the list"
+clear DOM of the list
+Use a for loop to loop through all past polls, create some DOM and display each past poll in the list
+Clear out/reset current poll state and DOM.
+current poll section
+Why? a bunch of empty divs that we will inject state into (question, options, and vote totals)
+empty div for closed polls history/list (for appending to)
+Why? A place to put the histry
--- app.js
+++ app.js
@@ -1,8 +1,100 @@
 // import functions and grab DOM elements
+import { renderPastPoll } from './render-utils.js';
+const pollQuestionEl = document.querySelector('#poll-question');
+const optionATitleEl = document.querySelector('#option-a-title');
+const optionBTitleEl = document.querySelector('#option-b-title');
+const optionAVotesEl = document.querySelector('#option-a-votes');
+const optionBVotesEl = document.querySelector('#option-b-votes');
+const optionAAddButton = document.querySelector('#option-a-add');
+const optionBAddButton = document.querySelector('#option-b-add');
+const optionAUndoButton = document.querySelector('#option-a-undo');
+const optionBUndoButton = document.querySelector('#option-b-undo');
+const endPollButton = document.querySelector('#end-poll');
+const form = document.querySelector('form');
+const pastPollsEl = document.querySelector('.past-polls');
+
 
 // let state
+let question = '';
+let optionAVotes = 0;
+let optionBVotes = 0;
+let optionATitle = '';
+let optionBTitle = '';
 
+const pastPollsArray = [];
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+optionAAddButton.addEventListener('click', () => {
+    optionAVotes++;
+    optionAVotesEl.textContent = optionAVotes;
+});
+
+optionBAddButton.addEventListener('click', () => {
+    optionBVotes++;
+    optionBVotesEl.textContent = optionBVotes;
+});
+
+optionAUndoButton.addEventListener('click', () => {
+    optionAVotes--;
+    optionAVotesEl.textContent = optionAVotes;
+});
+
+optionBUndoButton.addEventListener('click', () => {
+    optionBVotes--;
+    optionBVotesEl.textContent = optionBVotes;
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    question = data.get('poll-question');
+    optionATitle = data.get('poll-option-a');
+    optionBTitle = data.get('poll-option-b');
+    displayCurrentPoll();
+});
+
+endPollButton.addEventListener('click', () => {
+    form.reset();
+    const poll = makePoll();
+    pastPollsArray.push(poll);
+    resetState();
+    displayCurrentPoll();
+    displayList();
+});
+
+function makePoll() {
+    return {
+        question: question,
+        optionATitle: optionATitle,
+        optionBTitle: optionBTitle,
+        optionAVotes: optionAVotes,
+        optionBVotes: optionBVotes,
+    };
+}
+
+function resetState() {
+    question = '';
+    optionAVotes = 0;
+    optionBVotes = 0;
+    optionATitle = '';
+    optionBTitle = '';
+}
+
+function displayCurrentPoll() {
+    pollQuestionEl.textContent = question;
+    optionATitleEl.textContent = optionATitle;
+    optionBTitleEl.textContent = optionBTitle;
+    optionAVotesEl.textContent = optionAVotes;
+    optionBVotesEl.textContent = optionBVotes;
+}
+
+function displayList() {
+    pastPollsEl.textContent = '';
+
+    for (let pastPoll of pastPollsArray) {
+        const container = renderPastPoll(pastPoll);
+        pastPollsEl.append(container);
+    }
+}
